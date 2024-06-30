250b201c1164060803fb9f8dd59316a29f1daadc
Andryxa0333 <andron_0333@mail.ru>
2024-07-01 00:28:35 +0300

    Added solution code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Шаг первый
+string[] input = new string[4] {"Hello", "2", "world", ":-)"};
+string[] output = new string[input.Length];
+int elemCount = 0;
+
+// Шаг второй
+for (int i = 0; i < input.Length; i++){
+    if (input[i].Length <= 3){
+        output[elemCount] = input[i];
+        elemCount++;
+    }
+}
+
+// Шаг третий
+for (int i = 0; i < elemCount; i++){
+    Console.Write("{0} ",output[i]);
+}
